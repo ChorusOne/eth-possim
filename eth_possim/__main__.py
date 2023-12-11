0704d203fb375ee106b349268610e41352462afd
Enrique Fynn <fynn@chorus.one>
2023-12-11 19:02:17 +0000

    Add fee rewards manager to possim
    Adds ability to deploy compiled contracts, this should be favoured
    instead of compiling contracts with solc, it's more reliable because we
    can use the exact same bytecode that is deployed in the Ethereum
    blockchain.
    
    For the fee manager contract we need 2 contracts, first the library and
    then the main contract, we deploy the library contract and substitute a
    placeholder inside the compiled binary with the library's address.

--- eth_possim/__main__.py
+++ eth_possim/__main__.py
@@ -8,7 +8,7 @@ import eth_possim
 from eth_possim.config import initialise_privatenet
 from eth_possim.config.load import load_configuration
 from eth_possim.config.patch import patch_cl_cfg
-from eth_possim.contracts import deploy_contract_onchain
+from eth_possim.contracts import deploy_contract_onchain, deploy_compiled_contract
 from eth_possim.deposit import load_initial_deposit_tree
 from eth_possim.utils import ensure_dir_exists
 
@@ -73,6 +73,57 @@ def deploy_batch_deposit_contract(rpc: str):
     with open("./.data/configuration.yaml", "w") as f:
         yaml.dump(cfg, f)
 
+@cli.command()
+@click.option("--rpc", help="RPC endpoint URL address.", default="")
+def deploy_fee_manager_contracts(rpc: str):
+    """Deploys the fee manager contracts: library and contract."""
+
+    with open("./.data/configuration.yaml", "r") as f:
+        cfg = yaml.safe_load(f)
+    if not rpc:
+        rpc = f"http://localhost:{cfg['haproxy']['el']['port_geth_rpc']}"
+
+    fee_manager_library_address = deploy_compiled_contract(
+        cfg=cfg,
+        rpc=rpc,
+        foundry_json_path=f"{cfg['resources']}/ethereum_compiled_contracts/CalculateAndSendRewards.json",
+    )
+
+    # Patch and write back `configuration.yaml`
+    cfg["cl"]["fee_manager_library_address"] = fee_manager_library_address
+
+    fee_manager_address = deploy_compiled_contract(
+        cfg=cfg,
+        rpc=rpc,
+        foundry_json_path=f"{cfg['resources']}/ethereum_compiled_contracts/FeeRewardsManager.json",
+        args=[2800],
+        libraries=[("__$c56d76a1417c078a963cba4fa22c45184c$__", fee_manager_library_address)]
+    )
+    cfg["cl"]["fee_manager_address"] = fee_manager_address
+
+    with open("./.data/configuration.yaml", "w") as f:
+        yaml.dump(cfg, f)
+
+@cli.command()
+@click.option("--rpc", help="RPC endpoint URL address.", default="")
+@click.option("--path", help="Path to the contract source.")
+@click.option("--cfg-key-address", help="Key in which to save the contract address.")
+@click.option("--library", multiple=True, type=(str, str), help="Libraries to be replaced in the bytecode contract in the format key value")
+@click.argument("args", nargs=-1)
+def deploy_contract_bytecode(rpc: str, path: str, cfg_key_address: str, args: list, library: list):
+    """Deploys a contract by the compiled bytecode and abi."""
+
+    with open("./.data/configuration.yaml", "r") as f:
+        cfg = yaml.safe_load(f)
+    if not rpc:
+        rpc = f"http://localhost:{cfg['haproxy']['el']['port_geth_rpc']}"
+
+    contract_address = deploy_compiled_contract(cfg=cfg, rpc=rpc, foundry_json_path=path, args=args, libraries=library)
+
+    # Patch and write back `configuration.yaml`
+    cfg["cl"][cfg_key_address] = contract_address
+    with open("./.data/configuration.yaml", "w") as f:
+        yaml.dump(cfg, f)
 
 @cli.command()
 @click.option("--rpc", help="RPC endpoint URL address.", default="")
